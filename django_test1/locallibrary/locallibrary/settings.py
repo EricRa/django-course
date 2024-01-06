c308dd200a581fa2bdd610214183b8e8be44273a
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2024-01-06 14:31:18 -0500

    Start tutorial part 3

--- django_test1/locallibrary/locallibrary/settings.py
+++ django_test1/locallibrary/locallibrary/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add catalog application
+    'catalog.apps.CatalogConfig', # This was created in /catalog/apps.py
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Canada/Eastern'
 
 USE_I18N = True
 
